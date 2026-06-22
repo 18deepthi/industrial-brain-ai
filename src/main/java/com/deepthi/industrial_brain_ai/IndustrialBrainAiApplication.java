7787c255a4688ad326c925e676500cbdb29e32d2
Sai Venkata Deepthi <venkatasaideepthi3@gmail.com>
2026-06-22 14:00:51 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/deepthi/industrial_brain_ai/IndustrialBrainAiApplication.java
@@ -0,0 +1,13 @@
+package com.deepthi.industrial_brain_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialBrainAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustrialBrainAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepthi/industrial_brain_ai/IndustrialBrainAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepthi.industrial_brain_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustrialBrainAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
